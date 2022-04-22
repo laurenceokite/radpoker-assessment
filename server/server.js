a17cb9355e103491d9528c92216f77ba63e5f122
Laurence Okite <laurenceokite@Laurences-MacBook-Air.local>
2022-04-22 15:01:12 -0500

    try again

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(routes);
 app.use(express.static(path.join(__dirname, "../client/build")));
 
   
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
