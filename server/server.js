12d9e87b382057cdab6e7729b50c7ad10cec05f7
Laurence Okite <laurenceokite@Laurences-MacBook-Air.local>
2022-04-22 15:28:50 -0500

    updated server file

--- server/server.js
+++ server/server.js
@@ -10,11 +10,11 @@ app.use(express.json());
 app.use(routes);
 
 
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, 'build')));
 
-  
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+
+app.get('/*', function (req, res) {
+   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+ });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
